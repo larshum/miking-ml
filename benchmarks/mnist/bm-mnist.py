066ec63ab8deeddb11d5d8834d8cc671450f474a
John Wikman <john.wikman@hotmail.com>
2022-03-28 13:55:16 +0200

    pytorch-bm: add missing modules

--- benchmarks/mnist/bm-mnist.py
+++ benchmarks/mnist/bm-mnist.py
@@ -8,10 +8,10 @@ import os
 import sys
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 #import torch
-#import torch.nn as nn
-#import torch.nn.functional as F
 #import torch.optim as optim
 #from torchvision import datasets, transforms
 #from torch.optim.lr_scheduler import ExponentialLR
